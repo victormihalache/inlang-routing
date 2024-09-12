107326d6bf21f6cbf6525d024604802a4e5a9b30
Victor Mihalache <victormihalache@duck.com>
2024-09-12 13:42:03 +0200

    add inlang

--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+	import { i18n } from '$lib/i18n'
+import "../app.css";</script>
+<ParaglideJS {i18n}>
+<slot></slot>
+
+</ParaglideJS>
--- packages/app/src/routes/testi/+page.svelte
+++ packages/app/src/routes/testi/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  import * as m from "$lib/paraglide/messages";
+</script>
+
 <p>testi</p>
+<p>{m.welcome()}</p>
